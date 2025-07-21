7b3e7be66a9e85e4ff3564b875bb77fa6ba4b71f
Engy Emad <emadg9076@gmail.com>
2025-07-21 19:41:18 +0200

    first commit

--- src/components/Home/LoginPage.jsx
+++ src/components/Home/LoginPage.jsx
@@ -62,7 +62,8 @@ export default function LoginPage() {
 
         <p className="text-white text-center mt-3">
           Don't have an account?{' '}
-          <Link to="/register" className="text-warning">Register</Link>
+          <Link to="#/register" className="text-warning">Register</Link>
+
         </p>
       </div>
     </div>
