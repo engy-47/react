d90847b4f3b865e187c95d691d38a3915ce5698e
Engy Emad <emadg9076@gmail.com>
2025-07-21 18:28:08 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { CartProvider } from './components/Home/CartContext';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CartProvider>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </CartProvider>
   </React.StrictMode>
 );
